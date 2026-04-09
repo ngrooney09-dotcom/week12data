2d3c4fc11c8aaae8a8a9ee198b40b689697afb18
Long Nguyen <qnguyen78@my.bcit.ca>
2026-04-08 20:11:25 -0700

    tuff

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,13 +1,23 @@
 const database = require("mongoose");
-const is_hosted = process.env.IS_HOSTED || false;
-const databaseName = "lab_example"
+
+const is_hosted = process.env.IS_HOSTED === "true"; 
+const databaseName = "lab_example";
+
 const hostedURI =
-"mongodb+srv://monk:1234a@cluster0.wowgtvj.mongodb.net/lab_example?retryWrites=true&w=majority"
+"mongodb+srv://monk:1234a@cluster0.wowgtvj.mongodb.net/lab_example?retryWrites=true&w=majority";
+
 const localURI =
-"mongodb://localhost/"+databaseName+"?authSource=admin&retryWrites=true"
+"mongodb://localhost/" + databaseName + "?authSource=admin&retryWrites=true";
+
+console.log("IS_HOSTED:", process.env.IS_HOSTED);
+console.log("Using hosted DB:", is_hosted);
+
 if (is_hosted) {
-database.connect(hostedURI, {useNewUrlParser: true, useUnifiedTopology: true});
+  console.log("Connecting to ATLAS");
+  database.connect(hostedURI, { useNewUrlParser: true, useUnifiedTopology: true });
+} else {
+  console.log("Connecting to LOCAL");
+  database.connect(localURI, { useNewUrlParser: true, useUnifiedTopology: true });
 }
-else {
-database.connect(localURI, {useNewUrlParser: true, useUnifiedTopology: true});
-}
+
+module.exports = database;
