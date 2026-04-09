f468d37eca9caf79e9929500f0464bbd3968cda6
Long Nguyen <qnguyen78@my.bcit.ca>
2026-04-08 19:55:26 -0700

    dasf

--- databaseConnection.js
+++ databaseConnection.js
@@ -2,8 +2,7 @@ const database = require("mongoose");
 const is_hosted = process.env.IS_HOSTED || false;
 const databaseName = "lab_example"
 const hostedURI =
-"mongodb+srv://monk:1234a@cluster0.wowgtvj.mongodb.net/"+databas
-eName+"?retryWrites=true&w=majority"
+"mongodb+srv://monk:1234a@cluster0.wowgtvj.mongodb.net/lab_example?retryWrites=true&w=majority"
 const localURI =
 "mongodb://localhost/"+databaseName+"?authSource=admin&retryWrites=true"
 if (is_hosted) {
